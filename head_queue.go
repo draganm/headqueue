4a3bff69a0a5ceff448b30e5d0c02977a5e4ce01
Dragan Milic <dragan@netice9.com>
2026-01-21 22:40:50 +0100

    implement retry on locked db

--- head_queue.go
+++ head_queue.go
@@ -3,7 +3,8 @@ package headqueue
 import (
 	"context"
 	"database/sql"
-	"fmt"
+	"errors"
+	"strings"
 	"time"
 
 	"github.com/draganm/headqueue/sqlitestore"
@@ -24,74 +25,104 @@ func NewHeadQueue(
 	}
 }
 
+var ErrMaxBlocksBuffered = errors.New("max blocks buffered")
+
 func (hq *HeadQueue) Enqueue(ctx context.Context, block *sqlitestore.Block) error {
 
-	// wait for space to free up
-	{
-		queries := sqlitestore.New(hq.db)
+	queries := sqlitestore.New(hq.db)
+	for {
 
-		for {
+		err := func() error {
+
 			blockCount, err := queries.GetBlockCount(ctx)
 			if err != nil {
 				return err
 			}
-			if blockCount < int64(hq.maxBlocks) {
-				break
+
+			if blockCount >= int64(hq.maxBlocks) {
+				return ErrMaxBlocksBuffered
 			}
-			time.Sleep(1 * time.Second)
+
+			tx, err := hq.db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
+			if err != nil {
+				return err
+			}
+			defer tx.Rollback()
+
+			queries := sqlitestore.New(tx)
+			err = queries.InsertBlock(ctx, sqlitestore.InsertBlockParams{
+				Number:                  block.Number,
+				Hash:                    block.Hash,
+				Parent:                  block.Parent,
+				Block:                   block.Block,
+				Receipts:                block.Receipts,
+				CallTraces:              block.CallTraces,
+				PrestateTraces:          block.PrestateTraces,
+				Keccak256PreimageTraces: block.Keccak256PreimageTraces,
+				StateAccessTraces:       block.StateAccessTraces,
+			})
+			if err != nil {
+				return err
+			}
+
+			err = tx.Commit()
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}()
+
+		switch {
+		case err == ErrMaxBlocksBuffered, err != nil && strings.Contains(err.Error(), "database is locked"):
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-time.After(1 * time.Second):
+				continue
+			}
+		default:
+			return err
 		}
+
 	}
-
-	// TODO: implement backoff
-
-	tx, err := hq.db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
-
-	queries := sqlitestore.New(tx)
-	err = queries.InsertBlock(ctx, sqlitestore.InsertBlockParams{
-		Number:                  block.Number,
-		Hash:                    block.Hash,
-		Parent:                  block.Parent,
-		Block:                   block.Block,
-		Receipts:                block.Receipts,
-		CallTraces:              block.CallTraces,
-		PrestateTraces:          block.PrestateTraces,
-		Keccak256PreimageTraces: block.Keccak256PreimageTraces,
-		StateAccessTraces:       block.StateAccessTraces,
-	})
-	if err != nil {
-		return err
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func (hq *HeadQueue) PeekQueue(ctx context.Context, lastID int64) ([]sqlitestore.Block, error) {
-	// TODO: implement backoff when locked
 	queries := sqlitestore.New(hq.db)
-	blocks, err := queries.GetBlocksAfter(ctx, int64(lastID))
-	if err != nil {
-		return nil, err
+
+	for {
+		blocks, err := queries.GetBlocksAfter(ctx, int64(lastID))
+		switch {
+		case err != nil && strings.Contains(err.Error(), "database is locked"):
+			select {
+			case <-ctx.Done():
+				return nil, ctx.Err()
+			case <-time.After(1 * time.Second):
+				continue
+			}
+		default:
+			return blocks, nil
+		}
 	}
-
-	return blocks, nil
-
 }
 
 func (hq *HeadQueue) DropTail(ctx context.Context, id int64) error {
-	// TODO: implement backoff when locked
 	queries := sqlitestore.New(hq.db)
-	err := queries.DeleteBlocksBeforeIncluding(ctx, id)
-	if err != nil {
-		return fmt.Errorf("failed to delete blocks before including %d: %w", id, err)
+
+	for {
+		err := queries.DeleteBlocksBeforeIncluding(ctx, id)
+
+		switch {
+		case err != nil && strings.Contains(err.Error(), "database is locked"):
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-time.After(1 * time.Second):
+				continue
+			}
+		default:
+			return err
+		}
 	}
-	return nil
 }
